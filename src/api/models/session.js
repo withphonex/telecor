0c7513790ade92d12e9d61b32bd4d7066f6e1e9d
Ashley Narcisse-Austin <Ashley_NarcisseAustin@ultimatesoftware.com>
2018-12-01 16:26:06 -0500

    clean(model): cleaned up model

--- src/api/models/session.js
+++ src/api/models/session.js
@@ -1,28 +1,15 @@
 import mongoose, { Schema, Query } from '../../bootstrap';
 
-const Topic = new Schema({
-  title: String,
-  submitter: String,
-  created: { type: Date, default: Date.now },
-});
 
-//show comment if author or visibility set to true
 const Comment = new Schema({
   message: String,
   private: { type: Boolean, default: false },
   author: String,
-  emotion: {},
   created: { type: Date, default: Date.now },
 });
 
 export default mongoose.model('Session', new Schema({
   title: String,
-  employee: String,
-  manager: String,
-  employee_username: String,
-  manager_username: String,
-  employee_id: Number,
-  manager_id: Number,
   topics: [Topic],
   comments: [Comment],
   action_items: [String],
